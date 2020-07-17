8490332bda7082ada8c604fd5b7732abb3f9b09c
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-17 17:26:53 +0530

    Update run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -38,9 +38,9 @@ elif [ -n "${TOXENV}" ]; then
     tox -- --wycheproof-root="$HOME/wycheproof"
 else
     downstream_script="${TRAVIS_BUILD_DIR}/.travis/downstream.d/${DOWNSTREAM}.sh"
-    if [ ! -x "$downstream_script" ]; then
+    #if [ ! -x "$downstream_script" ]; then
         #exit 1
-    fi
+    #fi
     $downstream_script install
     pip install .
     $downstream_script run
