c5a66ef54c407d7d2c1eed607e0cb296ebde8973
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-17 17:24:43 +0530

    Update run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -39,7 +39,7 @@ elif [ -n "${TOXENV}" ]; then
 else
     downstream_script="${TRAVIS_BUILD_DIR}/.travis/downstream.d/${DOWNSTREAM}.sh"
     if [ ! -x "$downstream_script" ]; then
-        exit 1
+        #exit 1
     fi
     $downstream_script install
     pip install .
